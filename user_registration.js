5d24606b762a53e722f01c3a56d684c668727382
JasKaur96 <jaspreetflora7@gmail.com>
2021-04-02 22:35:43 +0530

    Added: Validated last name.

--- user_registration.js
+++ user_registration.js
@@ -1,10 +1,20 @@
 const readlineSync = require('readline-sync');
 let validation = function(){
-    //To check if name starts with Capital and has min 3 characters.
-    var fname = readlineSync.question("Enter Name :");
-    let nameRegex = /^[A-Z]{1}[a-z]{3,}/.test(fname);
-    if(nameRegex === true){
+    //To check if first name starts with Capital and has min 3 characters.
+    var fname = readlineSync.question("Enter First Name :");
+    let firstName = /^[A-Z]{1}[a-z]{3,}/.test(fname);
+    if(firstName === true){
         console.log("Validated First Name.");
+    }
+    else {
+        console.log("Name is incorrect!");
+    }
+
+    //To check if last name starts with Capital and has min 3 characters.
+    var lname = readlineSync.question("Enter Last Name :");
+    let lastName = /^[A-Z]{1}[a-z]{3,}/.test(lname);
+    if(lastName == true){
+        console.log("Validated Last Name.");
     }
     else {
         console.log("Name is incorrect!");
