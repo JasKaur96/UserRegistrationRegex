03e08c26679f49ab21cff9b66604abc76aeec707
JasKaur96 <77604638+JasKaur96@users.noreply.github.com>
2021-04-05 01:43:49 +0530

    Refactored: re-enter password if invalid.

--- user_registration.js
+++ user_registration.js
@@ -1,58 +1,46 @@
 const prompt = require('prompt-sync')({sigint: true}); 
+var regPattern;
+let namePattern = "^[A-Z]{1}[a-z]{3,}";
+let emailReg = "^[a-zA-Z0-9_.]+@[a-zA-Z.a-zA-Z{2,}.a-zA-Z{2,}]+$";
+let mobileReg = "^[9][1][ ][6-9]{1}[0-9]{9}$";
+let passwordReg = "^[A-Za-z]{8,}$";
 
-let validation = function(){
-    //To check if first name starts with Capital and has min 3 characters.
-    var fname = prompt("Enter First Name :");
-    let firstName = /^[A-Z]{1}[a-z]{3,}/.test(fname);
-    if(firstName === true){
-        console.log("Validated First Name.");
+validation = function(data,pattern){
+    regPattern = new RegExp(pattern);
+    result = regPattern.test(data);
+    if(result == true){
+        console.log("Validated.");
     }
     else {
-        console.log("First Name is incorrect!");
+        console.log("Please enter correct format!");
+        return false;
     }
-
-    //To check if last name starts with Capital and has min 3 characters.
-    var lname = prompt("Enter Last Name :");
-    let lastName = /^[A-Z]{1}[a-z]{3,}/.test(lname);
-    if(lastName == true){
-        console.log("Validated Last Name.");
-    }
-    else {
-        console.log("Last Name is incorrect!");
-    }
-
-     //To check if email is valid or not.
-     var email = prompt("Enter Email :");
-     let emailReg = /^[a-zA-Z0-9_.]+@[a-zA-Z.a-zA-Z{2,}.a-zA-Z{2,}]+$/.test(email);
-     if(emailReg == true){
-         console.log("Validated Email.");
-     }
-     else {
-         console.log("Email is incorrect!");
-     }
-
-    //To check if mobile number is valid or not.
-     var mobile = prompt("Enter Mobile No. :");
-     let mobileReg = /^[9][1][ ][6-9]{1}[0-9]{9}$/.test(mobile);
-     if(mobileReg == true){
-         console.log("Validated Mobile No..");
-     }
-     else {
-         console.log("Mobile No. is incorrect!");
-     }
-
-      //To check if password is valid or not with min 8 char.
-      var pswd = prompt("Enter Password :");
-      let password = /^[A-Za-z]{8,}$/.test(pswd);
-      if(password == true){
-          console.log("Validated Password.");
-      }
-      else {
-          console.log("Password is incorrect!");
-      }
 }
 
-validation();
+
+let getData = function(string, pattern){
+    let readData = prompt(string);
+    let result = validation(readData,pattern);
+  
+    while(result == false){
+        if(result == true){
+            return true; 
+        }
+        else{
+            let readData = prompt(string);
+            let result = validation(readData,pattern);
+            return result;
+        }
+       
+    }
+}
+
+getData('Enter the first name:',namePattern);
+getData('Enter the last name:',namePattern);
+getData('Enter the mobile no.:',mobileReg);
+getData('Enter the email:',emailReg);
+getData('Enter the password:',passwordReg);
+
 
 
 
