07c7b8671b63040d3f66e476e5a45724e99e0f33
JasKaur96 <jaspreetflora7@gmail.com>
2021-04-06 14:15:56 +0530

    Refactored: Added testcases for failing.

--- test/testUserRegisteration.js
+++ test/testUserRegisteration.js
@@ -5,17 +5,47 @@ describe('User Registeration Test',function(){
     it('first name', function(){
         assert.equal(data.firstName("Jaspreet"),true)  
     })
+    it('first name', function(){
+        assert.equal(data.firstName("jaspreet"),true)  
+    })
     it('last name', function(){
         assert.equal(data.lastName("Kaur"),true)  
+    })
+    it('last name', function(){
+        assert.equal(data.lastName("kaur"),true)  
     })
     it('mobile', function(){
         assert.equal(data.mobileNumber("91 9876677984"),true)  
     })
+    it('mobile', function(){
+        assert.equal(data.mobileNumber("919876677984"),true)  
+    })
+    it('mobile', function(){
+        assert.equal(data.mobileNumber("9876677984"),true)  
+    })
+    it('mobile', function(){
+        assert.equal(data.mobileNumber("+919876677984"),true)  
+    })
     it('email', function(){
         assert.equal(data.email("jas@gmail.com"),true)  
     })
+    it('email', function(){
+        assert.equal(data.email("jasgmail.com"),true)  
+    })
     it('password', function(){
         assert.equal(data.password("Jas@123abc"),true)  
+    })
+    it('password', function(){
+        assert.equal(data.password("Jas123abc"),true)  
+    })
+    it('password', function(){
+        assert.equal(data.password("Jas@abc"),true)  
+    })
+    it('password', function(){
+        assert.equal(data.password("jas@123abc"),true)  
+    })
+    it('password', function(){
+        assert.equal(data.password("J@123"),true)  
     })
     
 })
