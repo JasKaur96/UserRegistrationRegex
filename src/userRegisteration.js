f62b818c763710baca7f03e664dc41f0ab555890
JasKaur96 <77604638+JasKaur96@users.noreply.github.com>
2021-04-07 22:13:17 +0530

    Refactored: Removed display method

--- src/userRegisteration.js
+++ src/userRegisteration.js
@@ -111,10 +111,6 @@ let read = function(data){
 let printData = function(){
     console.log("\n\n*******UserDetails*******");
     console.log(readArr);
-    // console.log("FirstName :",readArr[0]);
-    // console.log("LastName :",readArr[1]);
-    // console.log("MobileNo. :",readArr[2]);
-    // console.log("Email :",readArr[3]);
 }
 
 getName('Enter First name:');
@@ -123,54 +119,6 @@ getMobileNo('Enter MobileNo.:');
 getEmail('Enter Email:');
 getPassword('Enter Password :');
 printData();
-
-
-
-
-// },
-// var Obj ={
-//     validation : function(data,pattern){
-//         regPattern = new RegExp(pattern);
-//         result = regPattern.test(data);
-//         return this;
-//     },
-
-//     validationPassed : function(){
-//         if(result == true){
-//             console.log("Validated.");
-//         }
-//         else {
-//             console.log("Please enter correct format!");
-//             getData();
-//         }
-//         return this;
-//     }
-// };
-
-    //     // To check if first name starts with Capital and has min 3 characters.
-    //     var fname = prompt("Enter First Name :");
-    //     Obj.validation(fname,namePattern).validationPassed();
-
-    //      // To check if first name starts with Capital and has min 3 characters
-    //     var lname = prompt("Enter Last Name :");
-    //     Obj.validation(lname,namePattern).validationPassed();
-       
-    //     //To check if email is valid or not.
-    //     var email = prompt("Enter Email :");
-    //     Obj.validation(email,emailReg).validationPassed();
-
-    //     //To check if mobile number is valid or not.
-    //     var mobile = prompt("Enter Mobile No. :");
-    //     Obj.validation(mobile,mobileReg).validationPassed();
-
-    //     /*To check if password is valid or not with min 8 char, at least 1 uppercase
-    //         atleast 1 numeric value and exact 1 special char.*/
-    //     var pswd = prompt("Enter Password :");
-    //     Obj.validation(pswd,passwordReg).validationPassed();
-
-    //     res = false;
-    // }
-// }
 
 
 
